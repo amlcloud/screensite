35f0b95d83dd5a2cfd6928a1732613aa34d06903
ClarieChek <chek.clarie@gmail.com>
2022-11-01 12:48:15 +1030

    Testing list page

--- lib/lists/list_list_item.dart
+++ lib/lists/list_list_item.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:screensite/lists/lists_page.dart';
 import 'package:screensite/providers/firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class ListItem extends ConsumerWidget {
   final String entityId;
@@ -12,25 +14,54 @@ class ListItem extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(docSP('list/' + entityId)).when(
-          loading: () => Container(),
-          error: (e, s) => ErrorWidget(e),
-          data: (entityDoc) => entityDoc.exists == false
-              ? Center(child: Text('No entity data exists'))
-              : ListTile(
-                  tileColor: Color.fromARGB(255, 44, 44, 44),
-                  focusColor: Color.fromARGB(255, 133, 116, 116),
-                  title: Text(
-                    entityDoc.data()!['name'] ?? 'name',
-                  ),
-                  trailing: Column(children: <Widget>[
-                    Text(entityDoc.data()!['id'] ?? 'id'),
-                  ]),
-                  subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
-                  onTap: () {
-                    ref.read(activeList.notifier).value = entityId;
-                  },
+        loading: () => Container(),
+        error: (e, s) => ErrorWidget(e),
+        data: (entityDoc) => entityDoc.exists == false
+            ? Center(child: Text('No entity data exists'))
+            : ListTile(
+                tileColor: Color.fromARGB(255, 44, 44, 44),
+                focusColor: Color.fromARGB(255, 133, 116, 116),
+                title: Text(
+                  entityDoc.data()!['uiName'] ?? 'name',
                 ),
-        );
+                // trailing: Column(children: <Widget>[
+                //   Text(entityDoc.data()!['id'] ?? 'id'),
+                // ]),
+                // subtitle: Text(entityDoc.data()!['desc'] ?? 'desc'),
+                subtitle: Material(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          child: Text('View source page'),
+                          onTap: () async {
+                            if (await canLaunchUrlString(
+                                entityDoc.data()!['domainURL'])) {
+                              await launchUrlString(
+                                  entityDoc.data()!['domainURL']);
+                            } else {
+                              throw 'Could not launch domain URL';
+                            }
+                          },
+                        ),
+                        InkWell(
+                          child: Text('View source list'),
+                          onTap: () async {
+                            if (await canLaunchUrlString(
+                                entityDoc.data()!['listURL'])) {
+                              await launchUrlString(
+                                  entityDoc.data()!['listURL']);
+                            } else {
+                              throw 'Could not launch list URL';
+                            }
+                          },
+                        ),
+                      ]),
+                ),
+                onTap: () {
+                  ref.read(activeList.notifier).value = entityId;
+                },
+              ));
   }
 
   Future<bool> CheckSelected() async {
