71666ba204a1f39c7c18759709b1d756a363c421
rahulkasana <rahulkasana@outlook.com>
2022-12-28 00:23:09 +1100

    UI: Configure entity name, address, datasource and website urls for a list - Show dialog box - No implementation

--- lib/lists/list_list_item.dart
+++ lib/lists/list_list_item.dart
@@ -95,7 +95,80 @@ class ListItem extends ConsumerWidget {
                                     print("URL can't be launched.");
                                   }
                                 })))
-                    ])
+                    ]),
+                InkWell(
+                    child: Icon(Icons.edit),
+                    // onTap: () {
+                    //   //ToDo
+                    //   print("Edit icon clicked");
+                    // },
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              scrollable: true,
+                              title: Text('Sanction list entity fields'),
+                              content: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Form(
+                                  child: Column(
+                                    children: <Widget>[
+                                      TextFormField(
+                                        // controller: id_inp,
+                                        decoration: InputDecoration(
+                                            labelText: 'Entity Name'),
+                                      ),
+                                      TextFormField(
+                                        // controller: name_inp,
+                                        decoration: InputDecoration(
+                                          labelText: 'Entity address',
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        // controller: desc_inp,
+                                        decoration: InputDecoration(
+                                          labelText: 'Data Source',
+                                        ),
+                                      ),
+                                      TextFormField(
+                                        // controller: desc_inp,
+                                        decoration: InputDecoration(
+                                          labelText: 'Website',
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                    child: Text("Done"),
+                                    onPressed: () {
+                                      // FirebaseFirestore.instance
+                                      //     .collection('batch')
+                                      //     .add({
+                                      //   'id': id_inp.text.toString(),
+                                      //   'name': name_inp.text.toString(),
+                                      //   'desc': desc_inp.text.toString(),
+                                      //   'time Created':
+                                      //       FieldValue.serverTimestamp(),
+                                      //   'author': FirebaseAuth
+                                      //       .instance.currentUser!.uid,
+                                      // }).then((value) => {
+                                      //           if (value != null)
+                                      //             {
+                                      //               FirebaseFirestore.instance
+                                      //                   .collection('batch')
+                                      //             }
+                                      //         });
+
+                                      // Navigator.of(context).pop();
+                                    })
+                              ],
+                            );
+                          });
+                    }),
               ])));
   }
 
