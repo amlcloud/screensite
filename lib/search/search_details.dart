30fa217b2d51a4fbf8aea35409336f9ab24e3b68
Robert Wan <robert.t.wan@accenture.com>
2023-02-06 11:22:44 +1100

    fixed a search issue

--- lib/search/search_details.dart
+++ lib/search/search_details.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:screensite/providers/firestore.dart';
+import 'package:screensite/search/search_results.dart';
 import 'package:screensite/state/generic_state_notifier.dart';
 import 'package:screensite/theme.dart';
 
@@ -33,10 +34,11 @@ class SearchDetails extends ConsumerWidget {
                     child: Column(
                   children: [
                     Text('Searched Target:${searchDoc.id}'), //kk
-                    Text(
-                        'Search Time:${Jiffy(searchDoc.data()!['timeCreated'].toDate()).format("h:mm a, do MMM, yyyy")}'),
+                    //     : Text(
+                    //         'Search Time:${Jiffy(searchDoc.data()!['timeCreated'].toDate()).format("h:mm a, do MMM, yyyy")}'),
                     //     'Search Time:${Jiffy(
-                    //               ).format("h:mm a, do MMM, yyyy")}'), // datetime format using jiffy                 SearchResults(searchDoc.id, _selectedItemNotifier)
+                    //               ).format("h:mm a, do MMM, yyyy")}'), // datetime format using jiffy
+                    SearchResults(searchDoc.id, _selectedItemNotifier)
                   ],
                 )));
           });
