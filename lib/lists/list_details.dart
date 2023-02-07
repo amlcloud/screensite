7b2c9c127ecaa0772cf0f5af1261e7b0bd4f978d
wtoledo <williamtoll99@gmail.com>
2023-02-08 02:26:19 +1100

    Removed unuse import

--- lib/lists/list_details.dart
+++ lib/lists/list_details.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:screensite/lists/list_info.dart';
