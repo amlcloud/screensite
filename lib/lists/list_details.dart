62fc4ba02b833d393e040e370181808ff5aebc49
kovarny <sivochkin@gmail.com>
2023-02-09 10:57:33 +1100

    fixed entity settings

--- lib/lists/list_details.dart
+++ lib/lists/list_details.dart
@@ -58,161 +58,7 @@ class ListDetails extends ConsumerWidget {
                     error: (e, s) => ErrorWidget(e),
                     data: (entityDoc) => entityDoc.exists == false
                         ? Center(child: Text('No entity data exists'))
-                        : Container(
-                            child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: [
-                                      IndexingProgress(entityId,
-                                          _indexButtonClicked.notifier)
-                                    ]),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: <Widget>[
-                                      Container(
-                                          padding: EdgeInsets.all(8.0),
-                                          child: Text.rich(TextSpan(
-                                              style: TextStyle(
-                                                  decoration:
-                                                      TextDecoration.underline),
-                                              //make link underline
-                                              text: "View source page",
-                                              recognizer: TapGestureRecognizer()
-                                                ..onTap = () async {
-                                                  //on tap code here, you can navigate to other page or URL
-                                                  final url = Uri.parse(
-                                                      entityDoc.data()![
-                                                              'domainURL'] ??
-                                                          '#');
-                                                  var urllaunchable =
-                                                      await canLaunchUrl(
-                                                          url); //canLaunch is from url_launcher package
-                                                  if (urllaunchable) {
-                                                    await launchUrl(
-                                                        url); //launch is from url_launcher package to launch URL
-                                                  } else {
-                                                    print(
-                                                        "URL can't be launched.");
-                                                  }
-                                                })))
-                                    ]),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    children: <Widget>[
-                                      Container(
-                                          padding: EdgeInsets.all(8.0),
-                                          child: Text.rich(TextSpan(
-                                              style: TextStyle(
-                                                  decoration:
-                                                      TextDecoration.underline),
-                                              //make link underline
-                                              text: "View source list",
-                                              recognizer: TapGestureRecognizer()
-                                                ..onTap = () async {
-                                                  //on tap code here, you can navigate to other page or URL
-                                                  final url = Uri.parse(
-                                                      entityDoc.data()![
-                                                              'listURL'] ??
-                                                          '#');
-                                                  var urllaunchable =
-                                                      await canLaunchUrl(
-                                                          url); //canLaunch is from url_launcher package
-                                                  if (urllaunchable) {
-                                                    await launchUrl(
-                                                        url); //launch is from url_launcher package to launch URL
-                                                  } else {
-                                                    print(
-                                                        "URL can't be launched.");
-                                                  }
-                                                })))
-                                    ]),
-                                InkWell(
-                                    child: Align(
-                                        alignment: Alignment
-                                            .centerLeft, //Alignment of Edit Pen icon by kk
-                                        child: IconButton(
-                                            icon: const Icon(Icons.edit),
-                                            onPressed: () {
-                                              showDialog(
-                                                  context: context,
-                                                  builder:
-                                                      (BuildContext context) {
-                                                    final _formKey =
-                                                        GlobalKey<FormState>();
-                                                    return AlertDialog(
-                                                      scrollable: true,
-                                                      title: Text(
-                                                          'Sanction list entity fields'),
-                                                      content: Padding(
-                                                        padding:
-                                                            const EdgeInsets
-                                                                .all(8.0),
-                                                        child: Form(
-                                                          key: _formKey,
-                                                          child: Column(
-                                                            children: <Widget>[
-                                                              DocFieldTextEdit(
-                                                                  FirebaseFirestore
-                                                                      .instance
-                                                                      .doc(
-                                                                          'list/${entityId}'),
-                                                                  'name',
-                                                                  decoration:
-                                                                      InputDecoration(
-                                                                          hintText:
-                                                                              "Entity Name")),
-                                                              DocFieldTextEdit(
-                                                                  FirebaseFirestore
-                                                                      .instance
-                                                                      .doc(
-                                                                          'list/${entityId}'),
-                                                                  'address',
-                                                                  decoration:
-                                                                      InputDecoration(
-                                                                          hintText:
-                                                                              "Entity address")),
-                                                              DocFieldTextEdit(
-                                                                  FirebaseFirestore
-                                                                      .instance
-                                                                      .doc(
-                                                                          'list/${entityId}'),
-                                                                  'dataSource',
-                                                                  decoration:
-                                                                      InputDecoration(
-                                                                          hintText:
-                                                                              "Data Source")),
-                                                              DocFieldTextEdit(
-                                                                  FirebaseFirestore
-                                                                      .instance
-                                                                      .doc(
-                                                                          'list/${entityId}'),
-                                                                  'website',
-                                                                  decoration:
-                                                                      InputDecoration(
-                                                                          hintText:
-                                                                              "Website")),
-                                                            ],
-                                                          ),
-                                                        ),
-                                                      ),
-                                                      actions: [
-                                                        ElevatedButton(
-                                                          onPressed: () {
-                                                            Navigator.of(
-                                                                    context)
-                                                                .pop();
-                                                          },
-                                                          child: const Text(
-                                                              'Done'),
-                                                        )
-                                                      ], //actions
-                                                    );
-                                                  } // Builder Widget
-                                                  ); //show dialog
-                                            }))),
-                              ])))),
+                        : buildListItemDetails(entityDoc, context))),
 
             Divider(),
             ListIndexing(entityId),
@@ -231,4 +77,128 @@ class ListDetails extends ConsumerWidget {
             Divider(),
             ListCount(entityId),
           ]));
+
+  Widget buildListItemDetails(
+      DocumentSnapshot<Map<String, dynamic>> entityDoc, BuildContext context) {
+    return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [IndexingProgress(entityId, _indexButtonClicked.notifier)]),
+      Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text.rich(TextSpan(
+                style: TextStyle(decoration: TextDecoration.underline),
+                //make link underline
+                text: "View source page",
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () async {
+                    //on tap code here, you can navigate to other page or URL
+                    final url =
+                        Uri.parse(entityDoc.data()!['domainURL'] ?? '#');
+                    var urllaunchable = await canLaunchUrl(
+                        url); //canLaunch is from url_launcher package
+                    if (urllaunchable) {
+                      await launchUrl(
+                          url); //launch is from url_launcher package to launch URL
+                    } else {
+                      print("URL can't be launched.");
+                    }
+                  })))
+      ]),
+      Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text.rich(TextSpan(
+                style: TextStyle(decoration: TextDecoration.underline),
+                //make link underline
+                text: "View source list",
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () async {
+                    //on tap code here, you can navigate to other page or URL
+                    final url = Uri.parse(entityDoc.data()!['listURL'] ?? '#');
+                    var urllaunchable = await canLaunchUrl(
+                        url); //canLaunch is from url_launcher package
+                    if (urllaunchable) {
+                      await launchUrl(
+                          url); //launch is from url_launcher package to launch URL
+                    } else {
+                      print("URL can't be launched.");
+                    }
+                  })))
+      ]),
+      InkWell(
+          child: Align(
+              alignment:
+                  Alignment.centerLeft, //Alignment of Edit Pen icon by kk
+              child: IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          final _formKey = GlobalKey<FormState>();
+                          return AlertDialog(
+                            scrollable: true,
+                            title: Text('Sanction list settings'),
+                            content: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: <Widget>[
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'entitiesName1',
+                                        decoration: InputDecoration(
+                                            hintText: "Entity Name 1")),
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'entitiesName2',
+                                        decoration: InputDecoration(
+                                            hintText: "Entity Name 2")),
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'name',
+                                        decoration: InputDecoration(
+                                            hintText: "List Name")),
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'entitiesAddress',
+                                        decoration: InputDecoration(
+                                            hintText: "Entity address")),
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'dataSource',
+                                        decoration: InputDecoration(
+                                            hintText: "Data Source")),
+                                    DocFieldTextEdit(
+                                        FirebaseFirestore.instance
+                                            .doc('list/${entityId}'),
+                                        'website',
+                                        decoration: InputDecoration(
+                                            hintText: "Website")),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actions: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Done'),
+                              )
+                            ], //actions
+                          );
+                        } // Builder Widget
+                        ); //show dialog
+                  }))),
+    ]);
+  }
 }
