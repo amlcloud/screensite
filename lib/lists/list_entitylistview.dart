52684bc8c3292eebe403e76ead792a1cc228fbd0
kovarny <sivochkin@gmail.com>
2023-03-30 11:13:33 +1100

    return sorting

--- lib/lists/list_entitylistview.dart
+++ lib/lists/list_entitylistview.dart
@@ -34,10 +34,8 @@ class _EntityListViewState extends ConsumerState<EntityListView> {
               loading: () => [],
               error: (e, s) => [],
               data: (listDoc) => ref
-                  .watch(
-                      colSPfiltered('list/${widget.entityId}/item', limit: 500
-                          //, orderBy: listDoc.data()?['entitiesName1']
-                          ))
+                  .watch(colSPfiltered('list/${widget.entityId}/item',
+                      limit: 500, orderBy: listDoc.data()?['entitiesName1']))
                   .when(
                       loading: () => [],
                       error: (e, s) => [ErrorWidget(e)],
