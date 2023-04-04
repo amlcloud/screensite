794684df2e8b034d27ccfa335c986a520e703fa0
Christopher Hullman <christopherhullman@gmail.com>
2023-04-04 11:53:00 +1000

    Removed unneeded import

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,9 +1,7 @@
 import 'package:auth/login_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:login/login.dart';
 import 'package:screensite/search/search_page.dart';
-
 
 class LoginPage extends ConsumerWidget {
   const LoginPage({Key? key}) : super(key: key);
