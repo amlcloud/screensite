bbbfa9bc6cbe5170c78401f38c7fceb6edd35ea2
mvemunoori-tonik <113677272+mvemunoori-tonik@users.noreply.github.com>
2023-06-13 10:17:12 +0530

    Added code for flutter automation

---
+++ integration_test/app_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:screensite/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter',
+        (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      print('***************');
+      final appUrl = Uri.base.toString();
+      print('Application URL: $appUrl');
+    });
+  });
+}
